3f6a9ad9bac1e3186eb48c6899c2c694ce2e0043
srikari <srikariupamaka@gmail.com>
2022-04-12 19:16:09 +0530

    first commit

---
+++ EmployeeService/src/main/resources/my.sql
@@ -0,0 +1,3 @@
+ insert into roles values('1','ADMIN');
+ INSERT INTO USERS VALUES('1','$2a$12$2W5NWM85WsCXZ0bUIU6DGuY6JJlzXXdHCIooRt983eMww1EbpsUD.','user1');
+ insert into users_roles values(1,1);
